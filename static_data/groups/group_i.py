f04995d6797f3da41e99a83f670e61fcdc63efb9
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:12:12 +0530

    Group I Committed

---
+++ static_data/groups/group_i.py
@@ -0,0 +1,9 @@
+GROUP_I = {
+    "name": "Group I",
+    "teams": (
+        {"name": "France", "flag": "🇫🇷"},
+        {"name": "Senegal", "flag": "🇸🇳"},
+        {"name": "Norway", "flag": "🇳🇴"},
+        {"name": "Iraq", "flag": "🇮🇶"},
+    ),
+}
