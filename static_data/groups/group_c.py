de32f349efb86f89a83daa3000213e84b04b92e7
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:10:03 +0530

    Group C Committed

---
+++ static_data/groups/group_c.py
@@ -0,0 +1,9 @@
+GROUP_C = {
+    "name": "Group C",
+    "teams": (
+        {"name": "Brazil", "flag": "🇧🇷"},
+        {"name": "Morocco", "flag": "🇲🇦"},
+        {"name": "Scotland", "flag": "🏴󠁧󠁢󠁳󠁣󠁴󠁿"},
+        {"name": "Haiti", "flag": "🇭🇹"},
+    ),
+}
