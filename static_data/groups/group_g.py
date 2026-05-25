cc8f91b3820c481836fb1c6b269abb56324d40be
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:11:29 +0530

    Group G Committed

---
+++ static_data/groups/group_g.py
@@ -0,0 +1,9 @@
+GROUP_G = {
+    "name": "Group G",
+    "teams": (
+        {"name": "Belgium", "flag": "🇧🇪"},
+        {"name": "Iran", "flag": "🇮🇷"},
+        {"name": "Egypt", "flag": "🇪🇬"},
+        {"name": "New Zealand", "flag": "🇳🇿"},
+    ),
+}
