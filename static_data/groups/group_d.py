7c7715ba43e64dddd35740c80dd987fdf8d87796
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:10:23 +0530

    Group D Committed

---
+++ static_data/groups/group_d.py
@@ -0,0 +1,9 @@
+GROUP_D = {
+    "name": "Group D",
+    "teams": (
+        {"name": "United States", "flag": "🇺🇸"},
+        {"name": "Paraguay", "flag": "🇵🇾"},
+        {"name": "Australia", "flag": "🇦🇺"},
+        {"name": "Turkiye", "flag": "🇹🇷"},
+    ),
+}
