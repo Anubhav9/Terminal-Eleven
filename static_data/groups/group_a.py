4527cf47fbe0d044f7a64f11dd25f48d8abb7437
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:09:13 +0530

    Group A Committed

---
+++ static_data/groups/group_a.py
@@ -0,0 +1,9 @@
+GROUP_A = {
+    "name": "Group A",
+    "teams": (
+        {"name": "Mexico", "flag": "🇲🇽"},
+        {"name": "South Africa", "flag": "🇿🇦"},
+        {"name": "South Korea", "flag": "🇰🇷"},
+        {"name": "Czechia", "flag": "🇨🇿"},
+    ),
+}
