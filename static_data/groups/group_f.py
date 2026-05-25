78eacbc6d6f59a112bbc3f0d0b2b16f3d04023b2
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:11:09 +0530

    Group F Committed

---
+++ static_data/groups/group_f.py
@@ -0,0 +1,9 @@
+GROUP_F = {
+    "name": "Group F",
+    "teams": (
+        {"name": "Netherlands", "flag": "🇳🇱"},
+        {"name": "Japan", "flag": "🇯🇵"},
+        {"name": "Tunisia", "flag": "🇹🇳"},
+        {"name": "Sweden", "flag": "🇸🇪"},
+    ),
+}
