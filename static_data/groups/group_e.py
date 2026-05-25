61409437caffc78a277585d09587b0f55da48d2e
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:10:47 +0530

    Group E Committed

---
+++ static_data/groups/group_e.py
@@ -0,0 +1,9 @@
+GROUP_E = {
+    "name": "Group E",
+    "teams": (
+        {"name": "Germany", "flag": "🇩🇪"},
+        {"name": "Ecuador", "flag": "🇪🇨"},
+        {"name": "Ivory Coast", "flag": "🇨🇮"},
+        {"name": "Curacao", "flag": "🇨🇼"},
+    ),
+}
