40445a0f6f442214be1e535174ba4ba7ff614df4
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:12:52 +0530

    group k committed

---
+++ static_data/groups/group_k.py
@@ -0,0 +1,9 @@
+GROUP_K = {
+    "name": "Group K",
+    "teams": (
+        {"name": "Portugal", "flag": "🇵🇹"},
+        {"name": "Colombia", "flag": "🇨🇴"},
+        {"name": "Uzbekistan", "flag": "🇺🇿"},
+        {"name": "DR Congo", "flag": "🇨🇩"},
+    ),
+}
