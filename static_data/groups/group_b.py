4b8a6d9a5684ebb00b7e73f04ec172130fcd674d
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:09:39 +0530

    Group B Committed

---
+++ static_data/groups/group_b.py
@@ -0,0 +1,9 @@
+GROUP_B = {
+    "name": "Group B",
+    "teams": (
+        {"name": "Canada", "flag": "🇨🇦"},
+        {"name": "Switzerland", "flag": "🇨🇭"},
+        {"name": "Qatar", "flag": "🇶🇦"},
+        {"name": "Bosnia and Herzegovina", "flag": "🇧🇦"},
+    ),
+}
