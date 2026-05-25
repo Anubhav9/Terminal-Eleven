986d9b0853a4664e1224b559a76f4b14df05be64
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:13:12 +0530

    group l committed

---
+++ static_data/groups/group_l.py
@@ -0,0 +1,9 @@
+GROUP_L = {
+    "name": "Group L",
+    "teams": (
+        {"name": "England", "flag": "🏴󠁧󠁢󠁥󠁮󠁧󠁿"},
+        {"name": "Croatia", "flag": "🇭🇷"},
+        {"name": "Ghana", "flag": "🇬🇭"},
+        {"name": "Panama", "flag": "🇵🇦"},
+    ),
+}
