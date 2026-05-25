837fca57a2a90c0c0fe72b09a04f5093611a4f19
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:12:32 +0530

    group j committed

---
+++ static_data/groups/group_j.py
@@ -0,0 +1,9 @@
+GROUP_J = {
+    "name": "Group J",
+    "teams": (
+        {"name": "Argentina", "flag": "🇦🇷"},
+        {"name": "Austria", "flag": "🇦🇹"},
+        {"name": "Algeria", "flag": "🇩🇿"},
+        {"name": "Jordan", "flag": "🇯🇴"},
+    ),
+}
