058689e0daaac9376a8791cbe7d5278a6b4f21a8
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-26 04:11:52 +0530

    Group H Committed

---
+++ static_data/groups/group_h.py
@@ -0,0 +1,9 @@
+GROUP_H = {
+    "name": "Group H",
+    "teams": (
+        {"name": "Spain", "flag": "🇪🇸"},
+        {"name": "Uruguay", "flag": "🇺🇾"},
+        {"name": "Saudi Arabia", "flag": "🇸🇦"},
+        {"name": "Cape Verde", "flag": "🇨🇻"},
+    ),
+}
